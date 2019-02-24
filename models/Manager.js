ce784f054c7cbd93a1414b90c52ffae96eaa07d1
mohamedalaaibrahim <mohamed.alaaibrahim@student.guc.edu.eg>
2019-02-24 19:22:01 +0200

    for khaled

---
+++ models/Manager.js
@@ -0,0 +1,16 @@
+const uuid = require('uuid')
+
+class Manager{
+    constructor(name, type, sex, nationality, identificationType, identificationNumber, birthDate, address, managerialPosition ){
+        this.name=name;
+        this.type=type;
+        this.sex=sex;
+        this.nationality=nationality;
+        this.identificationType=identificationType;
+        this.identificationNumber=identificationNumber;
+        this.birthDate=birthDate;
+        this.address=address;
+        this.managerialPosition=managerialPosition;
+        this.id = uuid.v4();
+    }
+}
