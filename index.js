bdd2690268651f4391aa225ee02f1551b0344684
ahmedreyad2019 <ahmedreyad2019@gmail.com>
2019-02-28 18:25:05 +0200

    comitting index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express')
-
 const lawyers = require('./routes/api/lawyer')
-
-
 const app = express()
 app.use(express.json())
-
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome to the Homepage/h1>
     <a href="/api/lawyer">Lawyers</a>
@@ -14,12 +10,9 @@ app.get('/', (req, res) => {
 
 // Direct routes to appropriate files 
 app.use('/api/lawyer', lawyers)
-
-
 // Handling 404
 app.use((req, res) => {
     res.status(404).send({err: 'We can not find what you are looking for'});
  })
-
 const port = 3000
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
