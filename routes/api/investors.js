d8da859e5a18b3022f74e7100ef09677e91b3d78
youssefAbobakr <youssefk.abobakr@gmail.com>
2019-02-28 20:36:47 +0200

    Investor CRUD

--- routes/api/investors.js
+++ routes/api/investors.js
@@ -60,8 +60,7 @@ const investors = [
 
 //View All Investors
 //router.get('/investors', (req, res) => res.send(investors));
-router.get('/', (req,res) => res.json({ data:investors}));
-
+router.get('/', (req, res) => res.json({ data:investors}));
 
 //View an Investor
 router.get('/:id', (req, res)=>
@@ -113,7 +112,84 @@ router.post('/create', (req, res) =>
 });
 
 
-module.exports = router;
+
+router.put('/:id', (req, res) => {
+    const index = req.params.id;
+
+    const inves = investors.find(investor=>investor.id==index);
+    
+    const name = req.body.name;
+    const type = req.body.type;
+    const gender= req.body.gender;
+    const nationality=req.body.nationality;
+    const idType=req.body.idType;
+    const idNumber=req.body.idNumber;
+    const dob=req.body.dob;
+    const address=req.body.address;
+    const telephone=req.body.telephone;
+    const fax=req.body.fax;
+    const mail=req.body.mail;
+    const password=req.body.password;
+
+    const schema = {
+        name:Joi.string(),
+        type:Joi.string(),
+        gender:Joi.string(),
+        nationality:Joi.string(),
+        idType:Joi.string(),
+        idNumber:Joi.number(),
+        dob:Joi.date(),
+        dob:Joi.string(),
+        address:Joi.string(),
+        telephone:Joi.number(),
+        fax:Joi.string(),
+        mail:Joi.string(),
+        password:Joi.string()
+        }
+    
+    const result= Joi.validate(req.body, schema);
+    if(result.error) return res.status(400).send({ error: result.error.details[0].message });
+    
+
+    if(name)
+        inves.name=name;
+    if(type)
+        inves.type=type;
+    if(gender)
+        inves.gender=gender;
+    if(nationality)
+        inves.nationalmineity=nationality;
+    if(idType)
+        inves.idType=idType;
+    if(idNumber)
+        inves.idNumber=idNumber;
+    if(dob)
+        inves.dob=dob;
+    if(address)
+        inves.address=address;
+    if(telephone)
+        inves.telephone=telephone;
+    if(fax)
+        inves.fax=fax;
+    if(mail)
+        inves.mail=mail;
+    if(password)
+        inves.password=password;
+    
+    res.send(investors);
+
+    
+
+})
+
+router.delete('/:id', (req, res) => {
+    const identifier = req.params.id 
+    const inves = investors.find(investor=>investor.id==identifier);
+    const index = investors.indexOf(inves);
+    investors.splice(index,1)
+    res.send(investors)
+})
 
 
 
+module.exports = router
