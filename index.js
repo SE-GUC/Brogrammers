afaf6a17a63c507669d844cfc519996e352532e2
andrew78041 <andrew.micheal@student.guc.edu.eg>
2019-03-02 17:06:56 +0200

    Company and Reviewer Updated

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express')
+const app = express()
 
-
-app.get('/', (req, res) => {
-    res.send(`<h1> Welcome To Reviewers Page</h1>
-    <a href ="/api/reviewer">reviewer</a>`);
-})
 
 const reviewer = require('../S1 complete  integrated with mangodb/routes/api/reviewer');
 const mongoose = require('mongoose')
 
 const companies = require('./routes/api/Company')
 //const books = require('./routes/api/Manager')
-
-const app = express()
 
 // DB Config
 const db = require('./config/keys').mongoURI
@@ -28,7 +22,8 @@ app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome to GAFI</h1>
-    <a href="/api/company">Companies</a>
+    <a href="/api/company">Companies</a> </br>
+    <a href ="/api/reviewer">reviewer</a>
     `);
 })
 
