502258d10d073da9eede10415c7b5936b7b47cac
BassemAyman2016 <47216060+BassemAyman2016@users.noreply.github.com>
2019-03-02 20:48:19 +0200

    Update updateValidation

--- validations/reviewerValidations.js
+++ validations/reviewerValidations.js
@@ -21,17 +21,17 @@ module.exports = {
 
     updateValidation: request => {
         const updateSchema = {
-            ssn: Joi.number().required(),
-            name: Joi.string().required(),
-            gender: Joi.string().required(),
-            address: Joi.string().required(),
-            phone: Joi.number().required(),
-            email: Joi.string().required(),
-            password: Joi.string().required(),
-            yearsOfExperience: Joi.number().required(),
-            age: Joi.number().required(),
-            birth: Joi.string().required(),
-            task: Joi.number().required()
+            ssn: Joi.number(),
+            name: Joi.string(),
+            gender: Joi.string(),
+            address: Joi.string(),
+            phone: Joi.number(),
+            email: Joi.string(),
+            password: Joi.string(),
+            yearsOfExperience: Joi.number(),
+            age: Joi.number(),
+            birth: Joi.string(),
+            task: Joi.number()
         }
 
         return Joi.validate(request, updateSchema)
