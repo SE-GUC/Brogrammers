cef09032a185e4f572f6b9cafe53e99505cd5ce6
Ahmad Hesham <ahmedhsaid@gmail.com>
2019-03-23 16:14:16 +0200

    before pull

---
+++ config/keys_dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://AhmadHesham:1q2w3e4r5t@manga-omnxn.mongodb.net/test?retryWrites=true'
+}
--- routes/api/admins.js
+++ routes/api/admins.js
@@ -110,24 +110,24 @@ router.put('/', async (req, res) => {
 router.post('/register', async (req, res) => {
   var stat = 0
   try {
-    var token = req.headers['x-access-token']
-    if (!token) {
-      return res
-        .status(401)
-        .send({ auth: false, message: 'Please login first.' })
-    }
-    jwt.verify(token, config.secret, async function (err, decoded) {
-      if (err) {
-        return res
-          .status(500)
-          .send({ auth: false, message: 'Failed to authenticate token.' })
-      }
-      stat = decoded.id
-    })
-    const admin2 = await Admin.findById(stat)
-    if (!admin2) {
-      return res.status(400).json({ error: 'You are not an admin' })
-    }
+    // var token = req.headers['x-access-token']
+    // if (!token) {
+    //   return res
+    //     .status(401)
+    //     .send({ auth: false, message: 'Please login first.' })
+    // }
+    // jwt.verify(token, config.secret, async function (err, decoded) {
+    //   if (err) {
+    //     return res
+    //       .status(500)
+    //       .send({ auth: false, message: 'Failed to authenticate token.' })
+    //   }
+    //   stat = decoded.id
+    // })
+    // const admin2 = await Admin.findById(stat)
+    // if (!admin2) {
+    //   return res.status(400).json({ error: 'You are not an admin' })
+    // }
     const {
       name,
       email,
