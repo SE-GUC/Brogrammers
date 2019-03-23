d5d24b559a84d24e6b739d6d19874b2044dcef3d
KhaledSerry <khaled_moataz2011@hotmail.com>
2019-03-23 20:39:32 +0200

    errors

--- routes/api/investors.js
+++ routes/api/investors.js
@@ -10,6 +10,17 @@ const Admin = require('../../models/Admin')
 const Company = require('../../models/Company')
 // View All Investors
 router.get('/', async (req, res) => {
+   var token = req.headers['x-access-token']
+  if (!token) {
+    return res.status(401).send({ auth: false, message: 'No Token provided.' })
+  }
+  jwt.verify(token, config.secret, function (err, decoded) {
+    if (err) {
+      return res
+        .status(500)
+        .send({ auth: false, message: 'Failed to authenticate token.' })
+    }
+  })
   const investors = await Investor.find()
   res.json({ data: investors })
 })
