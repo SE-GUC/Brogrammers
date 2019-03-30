9ecf0a30baf5ccbbe5013484f2314a9868062d65
Ahmad Hesham <ahmedhsaid@gmail.com>
2019-03-30 18:00:41 +0200

    Add full update testing

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const db = require('./config/keys').mongoURI
-const AdminsTest = require('./tests/admins')
-//const InvestorsTest = require('./tests/investors');
+//const AdminsTest = require('./tests/admins')
+const InvestorsTest = require('./tests/investors');
 
 const {
   PORT = 3000
@@ -25,13 +25,13 @@ mongoose.connect(db, {
 // ---== Core tests
 //= =---------------------------------------------------= =//
 //const lawyerTests = new LawyerTest(3000, 'lawyer')
-const adminsTests = new AdminsTest(3000, 'admins')
-//const investorTests = new InvestorsTest(3000, 'investors')
+//const adminsTests = new AdminsTest(3000, 'admins')
+const investorTests = new InvestorsTest(3000, 'investors')
 
 describe('Let me first run the independent tests', () => {
   Promise.all([
-    adminsTests.runTests(),
-    //investorTests.runTests(),
+    //adminsTests.runTests(),
+    investorTests.runTests(),
    // lawyerTests.runTests()
   ]).then(result => {
   })
