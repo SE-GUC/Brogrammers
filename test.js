ead4bd4f5827b2518079fcae2b3200b30032e0dc
KhaledSerry <khaled_moataz2011@hotmail.com>
2019-03-31 18:38:46 +0200

    testing half cruds on company

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 const db = require('./config/keys').mongoURI
 
-const LawyerTest = require('./tests/lawyers')
-//const InvestorsTest = require('./tests/investors')
-//const AdminsTest = require('./tests/admins')
+
+const CompanyTest=require('./tests/companys')
 
 mongoose.connect(db, {
   useNewUrlParser: true
@@ -21,16 +20,11 @@ afterAll(async () => {
 //= =---------------------------------------------------= =//
 // ---== Core tests
 //= =---------------------------------------------------= =//
-const lawyerTests = new LawyerTest(3000, 'lawyer')
-//const adminsTests = new AdminsTest(3000, 'admins')
-//const investorTests = new InvestorsTest(3000, 'investors')
+const companyTests = new CompanyTest(3000, 'company')
 
 describe('Let me first run the independent tests', () => {
   Promise.all([
-    //adminsTests.runTests(),
-    //investorTests.runTests(),
-    lawyerTests.runTests1(),
-    lawyerTests.runTests2()
+    companyTests.runTests()
   ]).then(result => {
   })
 })
