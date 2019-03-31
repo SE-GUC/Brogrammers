fe278a33f5980b6162b6436078001fed5dc729c7
MohamedAlaaIbrahim <mohamedalaaabdelhamid@gmail.com>
2019-03-31 03:03:37 +0200

    Lawyer company Creation tests

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const db = require('./config/keys').mongoURI
 
 const LawyerTest = require('./tests/lawyers')
-const InvestorsTest = require('./tests/investors')
+//const InvestorsTest = require('./tests/investors')
 //const AdminsTest = require('./tests/admins')
 
 mongoose.connect(db, {
@@ -23,13 +23,14 @@ afterAll(async () => {
 //= =---------------------------------------------------= =//
 const lawyerTests = new LawyerTest(3000, 'lawyer')
 //const adminsTests = new AdminsTest(3000, 'admins')
-const investorTests = new InvestorsTest(3000, 'investors')
+//const investorTests = new InvestorsTest(3000, 'investors')
 
 describe('Let me first run the independent tests', () => {
   Promise.all([
     //adminsTests.runTests(),
-    investorTests.runTests(),
-    lawyerTests.runTests()
+    //investorTests.runTests(),
+    lawyerTests.runTests1(),
+    lawyerTests.runTests2()
   ]).then(result => {
   })
 })
