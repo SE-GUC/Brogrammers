7f558527adc266799de1f6b79f0aa5b4b126f4a3
Fatairy <omar.elfatairy@student.guc.edu.eg>
2019-04-04 14:39:27 +0200

    new file please work

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Navbar from  './components/layout/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+    <Navbar/>
+
+     
+    );
+  }
+}
+
+export default App;
