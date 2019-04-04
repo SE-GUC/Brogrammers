db075d0b43248b3d5d904e5c2e0d3f9a8e63f6d8
MoAtefAmer <mo.atefamer@gmail.com>
2019-04-04 22:35:17 +0200

    added a button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,41 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Navbar from  './components/layout/Navbar';
-
+import Buttons from './components/buttons/Button'
 class App extends Component {
+
+  state={
+    lawyers:
+    [
+    {
+      lawyerId:"5ca651d277a5e6239428b626"
+    }
+  ],
+companys:
+[
+  {
+    companyId:"5ca6558c77a5e6239428b62d"
+  }
+]
+
+  }
+
+
+
+
+
   render() {
+
+   
     return (
-    <Navbar/>
+      <div>
+<Navbar/>
+<Buttons lawyers={this.state.lawyers}></Buttons>
+
+
+
+      </div>
+    
 
      
     );
