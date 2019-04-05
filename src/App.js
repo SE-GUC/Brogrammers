791ade210cf1b1255b0e49abfb2141f4703c18ab
KhaledSerry <khaled_moataz2011@hotmail.com>
2019-04-05 11:12:29 +0200

    adding some components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Navbar from  './components/layout/Navbar';
+import Register from  './components/pages/Register';
+import 'typeface-roboto';
+import Input from  './components/layout/inputs/Input';
 
 class App extends Component {
   render() {
     return (
+      <React.Fragment>
     <Navbar/>
-
-     
+    <Register/>
+     </React.Fragment>
     );
   }
 }
