6b4cbca1855c9f5d0429b68d83bf9b5e49a49848
MoAtefAmer <mo.atefamer@gmail.com>
2019-04-07 21:19:36 +0200

    beautified the view tasks

--- src/components/pages/LawyerCases.js
+++ src/components/pages/LawyerCases.js
@@ -2,7 +2,8 @@
 import React, { Component } from 'react';
 import SimpleCard from '../cards/SimpleCard'
 
-class LawyerCases extends React.Component{
+
+class LawyerCases extends Component{
     constructor(props) {
         super(props);
         this.state = {
@@ -11,41 +12,71 @@ class LawyerCases extends React.Component{
     }
 }
 
-   fetchLawyerCases(){
-        fetch('http://localhost:3000/api/lawyer/getall/cases',{
-          headers: new Headers({
-       //    'x-access-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjYThkZTE1ZWJjZDYwMWE2MDBhNzk3MSIsImlhdCI6MTU1NDU3MDc3OSwiZXhwIjoxNTU0NjU3MTc5fQ.3NumY5TJkdIDbtAGl1jcdZnVPdq2Saqh4QKBgvkNhF8'
-        //if login setted token correctly this should work
-        'x-access-token':this.props.token
-        })
-        })
-    
-        .then(res=> res.json())
-        .then(json=>{
-          this.setState({
-            lawyerCases:json.data
-          })
-          
-        })
-     
-    }
+   
     componentDidMount(){
       
-        this.fetchLawyerCases()
+        {
+            fetch('http://localhost:3000/api/lawyer/getall/cases',{
+              headers: new Headers({
+         //   'x-access-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjYTllN2VlN2E0MDNjMDc3YzM3ZGNkMSIsImlhdCI6MTU1NDYzODgzMCwiZXhwIjoxNTU0NzI1MjMwfQ.bEFsxvXTTktmM0JxCPxaiqxmPd3WpjIJRrOEFOfVI8M'
+            //if login setted token correctly this should work
+            'x-access-token':this.props.token
+            })
+            })
+        
+            .then(res=> res.json())
+            .then(json=> {
+              this.setState({
+                lawyerCases:json.data
+              })
+              
+            });
+         
+        }
        
         }
    
 
     
     render(){
-        console.log(this.state.lawyerCases)
-       
+       console.log(this.state.lawyerCases)
+        
         return(
            <div>
-    =
+    
+ 
+<ul>
+{this.state.lawyerCases.map((item)=> (
+    <SimpleCard  key={item._id} nameInEnglish={item.nameInEnglish} status={item.status}
+    addressHQ= {item.addressHQ}
+    Status= {item.status}
+    addressHQ= {item.addressHQ}
+    regulationLaw= {item.regulationLaw} 
+legalCompanyForm= {item.legalCompanyForm}
+nameInArabic= {item.nameInArabic}
+ governerateHQ={item.governerateHQ}
+cityHQ= {item.cityHQ}
 
-
-        <SimpleCard t={this.state.lawyerCases} />
+telephoneHQ= {item.telephoneHQ}
+faxHQ= {item.faxHQ}
+capitalCurrency= {item.capitalCurrency}
+capital= {item.capital}
+investorName={item.investorName}
+investorSex={item.investorSex} 
+investorNationaty= {item.investorNationaty}
+investorIdentificationType= {item.investorIdentificationType}
+investorIdentificationNumber= {item.investorIdentificationNumber}
+investorBD= {item.investorBD}
+investorAddress= {item.investorAddress}
+investorTelephone= {item.investorTelephone}
+investorFax= {item.investorFax}
+investorEmail={item.investorEmail}
+    
+    />
+    
+  
+))}
+       </ul>
    
 
 </div>
