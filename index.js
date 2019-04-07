99f3d4fcb669c0e47ae1a1c4568f6d81c498f1e9
Ahmad Hesham <ahmedhsaid@gmail.com>
2019-04-07 21:12:37 +0200

    PUT working with cors

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const investors = require('./routes/api/investors')
 const cors = require('cors')
 
 app.use(cors())
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+     next();
+});
 // DB Config
 const db = require('./config/keys').mongoURI
 const dotenv=require('dotenv')
