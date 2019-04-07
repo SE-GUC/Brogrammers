497acb080eadbdd6e477a26db5b53cc8f1e29b68
Mohamed Atef <47057819+MoAtefAmer@users.noreply.github.com>
2019-04-07 12:05:29 +0200

    getting token from login

--- src/components/pages/LawyerCases.js
+++ src/components/pages/LawyerCases.js
@@ -15,7 +15,7 @@ class LawyerCases extends React.Component{
         fetch('http://localhost:3000/api/lawyer/getall/cases',{
           headers: new Headers({
        //    'x-access-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjYThkZTE1ZWJjZDYwMWE2MDBhNzk3MSIsImlhdCI6MTU1NDU3MDc3OSwiZXhwIjoxNTU0NjU3MTc5fQ.3NumY5TJkdIDbtAGl1jcdZnVPdq2Saqh4QKBgvkNhF8'
-        //hardcoding token for now till login is completed 
+        //if login setted token correctly this should work
         'x-access-token':this.props.token
         })
         })
