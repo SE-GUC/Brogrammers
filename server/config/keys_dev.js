11c7f616d6765c5a4cb99d83545aa5989db1bb0f
youssefwally <yussufwaly3@gmail.com>
2019-04-13 16:40:00 +0200

    Updated login to not give type right away

--- server/config/keys_dev.js
+++ server/config/keys_dev.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://ahmedreyad2019:ahmedahmedahmed@cluster0-cafge.mongodb.net/test?retryWrites=true'
+  //mongoURI: 'mongodb+srv://ahmedreyad2019:ahmedahmedahmed@cluster0-cafge.mongodb.net/test?retryWrites=true'
+   mongoURI: 'mongodb+srv://waly:walytestse@cluster0-xho3f.mongodb.net/test?retryWrites=true'
 
 }
