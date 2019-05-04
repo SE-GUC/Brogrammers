a36b329efddacdef4039e7f750bdb0b40df85f5b
KhaledSerry <khaled_moataz2011@hotmail.com>
2019-05-04 21:00:14 +0200

    Paying again and some UI edits

--- server/config/keys_dev.js
+++ server/config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
  // mongoURI: 'mongodb+srv://andrew78041:571998@cluster0-cafge.mongodb.net/test?retryWrites=true'
- mongoURI: 'mongodb+srv://ahmedreyad2019:ahmedahmedahmed@cluster0-cafge.mongodb.net/test?retryWrites=true'
+ mongoURI: 'mongodb+srv://khaled:123@brogrammes-lkrkx.mongodb.net/test?retryWrites=true'
  // mongoURI: 'mongodb+srv://AhmadHesham:1q2w3e4r5t@manga-omnxn.mongodb.net/test?retryWrites=true'
 }
